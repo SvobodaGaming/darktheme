805c47b55b329916984459c5b5c86f11891e77a5
Svoboda.Gaming <medmish6@gmail.com>
2022-03-17 22:25:29 +0300

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -4,14 +4,16 @@ let lightCSS = "css/light.css";  // light css destination
 let darkCSS = "css/dark.css"; // dark css destination
 
 let useSystemTheme = true; // if true, script start using system theme
-let useTimeToChange = false; // if true, disable 'useSystemtime' option
+let useTime = false; // if true, disable 'useSystemtime' option
 
 if (useSystemTheme == true) {
 	let currentTheme = window.matchMedia("(prefers-color-scheme: dark)");
-	useTimeToChange = false;
+	useTime = false;
 }
 
-if (useTimeToChange == true) {
+if (useTime == true) {
+	let date = new Date();
+	let time = date.getHours();
 	let timeLimits = {
 		startLightAt: 8,
 		startDarkAt: 22
@@ -21,7 +23,7 @@ if (useTimeToChange == true) {
 let head = Document.getElementsByTagName("head")[0];
 let link = Document.createElement("link")[0];
 
-link.href = lightCSS; // or 'darkCSS' (by default)
+link.href = lightCSS; // default theme (for default: lightCSS)
 link.rel = "stylesheet";
 
 if (useSystemTheme == true) {
@@ -31,6 +33,11 @@ if (useSystemTheme == true) {
 		link.href = lightCSS;
 	}
 } else {
-	
+	if (useTime == true) {
+		if (timeLimits.startLightAt >= time && timeLimits.startDarkAt <= time) {
+			link.href = lightCSS;
+		} else {
+			link.href = darkCSS;
+		}
+	}
 }
-
